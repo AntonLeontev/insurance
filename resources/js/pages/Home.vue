fbae7a75d6ae48b640347e247ee5566121162bed
Anton <aner-anton@yandex.ru>
2025-02-02 15:48:32 +0300

    change: home

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
     import AppLayout from '@/layouts/AppLayout.vue';
 	import CrudPage from '@/components/CrudPage.vue';
+	import H1 from '@/components/H1.vue';
 
 </script>
 
@@ -8,9 +9,9 @@
     <AppLayout>
 		<CrudPage>
 			<template v-slot:header>
+				<H1>Dashboard</H1>
 			</template>
 			<template v-slot:content>
-			Home
 			</template>
 		</CrudPage>
     </AppLayout>
