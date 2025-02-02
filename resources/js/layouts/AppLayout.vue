b8bf775224d616d661bbcde0dbe15fae4cbeb6b8
Anton <aner-anton@yandex.ru>
2025-02-02 15:35:16 +0300

    change: main menu icons

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -18,25 +18,25 @@ const user = useUserStore().user;
             <v-list>
                 <v-list-item>
                     <RouterLink :to="{ name: 'home' }" class="d-flex ga-1" :class="$route.name === 'home' ? 'text-primary' : ''">
-						<v-icon icon="mdi-file-send-outline"></v-icon>
+						<v-icon icon="mdi-home-circle-outline"></v-icon>
 						Главная
 					</RouterLink>
                 </v-list-item>
                 <v-list-item v-if="user.role === 'admin'">
                     <RouterLink :to="{ name: 'users' }" class="d-flex ga-1" :class="$route.name === 'users' ? 'text-primary' : ''">
-						<v-icon icon="mdi-file-send-outline"></v-icon>
+						<v-icon icon="mdi-account-group"></v-icon>
 						Пользователи
 					</RouterLink>
                 </v-list-item>
                 <v-list-item v-if="user.role === 'admin'">
                     <RouterLink :to="{ name: 'agency-settings' }" class="d-flex ga-1" :class="$route.name === 'agency-settings' ? 'text-primary' : ''">
-						<v-icon icon="mdi-file-send-outline"></v-icon>
+						<v-icon icon="mdi-cog"></v-icon>
 						Настройки агенства
 					</RouterLink>
                 </v-list-item>
                 <v-list-item v-if="user.role === 'admin'">
                     <RouterLink :to="{ name: 'atol-settings' }" class="d-flex ga-1" :class="$route.name === 'atol-settings' ? 'text-primary' : ''">
-						<v-icon icon="mdi-file-send-outline"></v-icon>
+						<v-icon icon="mdi-cash-register"></v-icon>
 						Настройки АТОЛ
 					</RouterLink>
                 </v-list-item>
@@ -54,19 +54,19 @@ const user = useUserStore().user;
                 </v-list-item>
                 <v-list-item>
                     <RouterLink :to="{ name: 'receipts.drafts' }" class="d-flex ga-1" :class="$route.name === 'receipts.drafts' ? 'text-primary' : ''">
-						<v-icon icon="mdi-file-send-outline"></v-icon>
+						<v-icon icon="mdi-receipt-clock-outline"></v-icon>
 						Черновики чеков
 					</RouterLink>
                 </v-list-item>
                 <v-list-item>
                     <RouterLink :to="{ name: 'receipts.submitted' }" class="d-flex ga-1" :class="$route.name === 'receipts.submitted' ? 'text-primary' : ''">
-						<v-icon icon="mdi-file-send-outline"></v-icon>
+						<v-icon icon="mdi-receipt-text-check"></v-icon>
 						Оформленные чеки
 					</RouterLink>
                 </v-list-item>
                 <v-list-item>
                     <RouterLink :to="{ name: 'receipts.create' }" class="d-flex ga-1" :class="$route.name === 'receipts.create' ? 'text-primary' : ''">
-						<v-icon icon="mdi-plus"></v-icon>
+						<v-icon icon="mdi-receipt-text-plus"></v-icon>
 						Новый чек
 					</RouterLink>
                 </v-list-item>
