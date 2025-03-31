e3ed39fd3b35d1a8ac1392ef5db1438eb00f14f3
Anton <aner-anton@yandex.ru>
2025-03-31 20:12:58 +0300

    fix: api

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\ReceiptController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
-    Route::get('/receipts', [ReceiptController::class, 'index']);
+    Route::get('/receipts', [ReceiptController::class, 'index'])
+        ->middleware(['needAgency']);
 });
