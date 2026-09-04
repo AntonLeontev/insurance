76620b0fe52e65ab9f07965a0d18c8b0977a0c83
Anton <aner-anton@yandex.ru>
2026-09-04 22:57:30 +0300

    change: console.php

--- routes/console.php
+++ routes/console.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('snapshot:create')->daily();
+Schedule::command('snapshot:create --compress')->daily();
 Schedule::command('snapshot:cleanup --keep=7')->daily();
